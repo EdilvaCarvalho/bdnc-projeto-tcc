d78423c0711430adbdac50d1eaad76304a853430
Edilva Carvalho <edilva.intin@gmail.com>
2017-10-18 17:24:34 -0300

    Correção de Falha

--- src/main/java/br/edu/ifpb/bdnc/projeto/tcc/domain/entidades/Usuario.java
+++ src/main/java/br/edu/ifpb/bdnc/projeto/tcc/domain/entidades/Usuario.java
@@ -1,4 +1,3 @@
-
 package br.edu.ifpb.bdnc.projeto.tcc.domain.entidades;
 
 import br.edu.ifpb.bdnc.projeto.tcc.util.ConvertLocalDate;
@@ -18,39 +17,31 @@ import javax.persistence.SequenceGenerator;
  * @author Edilva
  */
 @Entity
-@SequenceGenerator(name = "minha_seq",
-        sequenceName = "MinhaSuperSequence",
-        allocationSize = 1)
-public class Usuario implements Serializable{
-    
+public class Usuario implements Serializable {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "minha_seq")
+    @GeneratedValue
     @Column(name = "id")
     private int id;
-    @Column(name = "nome",nullable=false, length=80)
+    @Column(name = "nome", nullable = false, length = 80)
     private String nome;
-    @Column(name = "dataNasc",nullable=false)
-    @Convert(converter = ConvertLocalDate.class)
-    private LocalDate dataNascimento;
-    @Column(name = "email",nullable=false, length=80, unique = true)
+    @Column(name = "email", nullable = false, length = 80, unique = true)
     private String email;
-    @Column(name= "senha",nullable=false, length=30)
+    @Column(name = "senha", nullable = false, length = 30)
     private String senha;
 
     public Usuario() {
     }
 
-    public Usuario(String nome, LocalDate dataNascimento, String email, String senha) {
+    public Usuario(String nome, String email, String senha) {
         this.nome = nome;
-        this.dataNascimento = dataNascimento;
         this.email = email;
         this.senha = senha;
     }
 
-    public Usuario(int id, String nome, LocalDate dataNascimento, String email, String senha) {
+    public Usuario(int id, String nome, String email, String senha) {
         this.id = id;
         this.nome = nome;
-        this.dataNascimento = dataNascimento;
         this.email = email;
         this.senha = senha;
     }
@@ -69,14 +60,6 @@ public class Usuario implements Serializable{
 
     public void setNome(String nome) {
         this.nome = nome;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
     }
 
     public String getEmail() {
@@ -100,7 +83,6 @@ public class Usuario implements Serializable{
         int hash = 3;
         hash = 29 * hash + this.id;
         hash = 29 * hash + Objects.hashCode(this.nome);
-        hash = 29 * hash + Objects.hashCode(this.dataNascimento);
         hash = 29 * hash + Objects.hashCode(this.email);
         hash = 29 * hash + Objects.hashCode(this.senha);
         return hash;
@@ -130,15 +112,12 @@ public class Usuario implements Serializable{
         if (!Objects.equals(this.senha, other.senha)) {
             return false;
         }
-        if (!Objects.equals(this.dataNascimento, other.dataNascimento)) {
-            return false;
-        }
         return true;
     }
 
     @Override
     public String toString() {
-        return "Usuario{" + "id=" + id + ", nome=" + nome + ", dataNascimento=" + dataNascimento + ", email=" + email + ", senha=" + senha + '}';
+        return "Usuario{" + "id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + '}';
     }
-    
+
 }
